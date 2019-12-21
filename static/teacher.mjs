33531666476db8a990ec859a63fbfe3e74783686
devmentality <nikita.volnov.1999@mail.ru>
2019-12-21 16:39:39 +0500

    example

--- static/teacher.mjs
+++ static/teacher.mjs
@@ -17,6 +17,24 @@ const connect = () => {
     return new WebSocket(url);
 };
 
+function createStudentElement(student)
+{
+    return `
+    <div id="div${student.role}" class="form-row" style="margin: 30px">
+        <div class="col">
+            <label>${student.name}</label>
+        </div>
+        <div class="col" name="name${student.role}">
+            <input class="btn" style="background-color: greenyellow" type="submit" value="Вопрос">
+            <input class="btn" style="background-color: red" type="submit" value="X">
+        </div>
+        <div class="col" name="name${student.role}">
+            <input class="btn" style="background-color: blue" type="submit" value="Ответ">
+            <input class="btn" style="background-color: red" type="submit" value="X">
+        </div>
+    </div>`;
+}
+
 function handleNewStudent (student) {
 
 }
