3444a20cfd5cf0d2efdf26a002c79bc2e575609e
alexbagirov <a.d.bagirov@gmail.com>
2019-12-21 13:18:00 +0500

    Add rooms

---
+++ room.js
@@ -0,0 +1,10 @@
+import Student from 'users/student';
+import Teacher from 'users/teacher';
+
+class Room {
+    constructor(teacher) {
+        this.id = Math.random().toString(36).substr(2, 9);
+        this.teacher = teacher;
+        this.users = [];
+    }
+}
